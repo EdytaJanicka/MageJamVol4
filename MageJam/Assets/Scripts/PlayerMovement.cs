0902b9f92b3f8d3f3a8ae527df9e70bee42b3a94
EdytaJanicka <edyta.janicka0@gmail.com>
2021-03-06 00:38:13 +0100

    faw

---
+++ MageJam/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public GameObject character;
+    public float speed = 0.5f;
+
+    
+
+
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        Vector3 characterScale = character.transform.localScale;
+        
+        float gravity = 9.8f;
+        Vector3 moveAxis = new Vector3(horizontal, -gravity, vertical);
+        controller.Move(((moveAxis) * speed * Time.deltaTime));
+        
+        if (horizontal > 0)
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.right), 3f * Time.deltaTime);
+        else if (horizontal < 0)
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.left), 3f * Time.deltaTime);
+        if (vertical > 0)
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.forward), 3f * Time.deltaTime);
+        else if (vertical < 0)
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.back), 3f * Time.deltaTime);
+
+    }
+
+
+}
